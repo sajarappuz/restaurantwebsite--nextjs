edd8ce597147a53b8aa9eec6b6362fb32573782f
Sajarappuz <sajarmohammed71@gmail.com>
2023-10-21 20:39:14 +0530

    final

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
 import Footer from '@/components/Footer'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import Headline from '@/components/Headline'
 import Navbar from '@/components/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body >
         <div>
           <Headline/>
           <Navbar/>
